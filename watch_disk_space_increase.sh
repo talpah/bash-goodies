beea077358d159413d32cf60f113e56f781718ca
Cosmin I <phprecode@gmail.com>
2013-10-24 17:16:13 +0300

    Update watch_disk_space_increase.sh

--- watch_disk_space_increase.sh
+++ watch_disk_space_increase.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-iterations=0; 
-firstsize=0; 
-oldsize=0; 
-while true; do 
-	newsize=`df /dev/sda1 | grep -v 'Filesystem' | cut -d ' ' -f 3`; 
-	
-	if [[ firstsize -eq 0 ]]; then 
-		firstsize=$newsize; 
-	fi;  
-	
-	((diffsize=newsize - oldsize)); 
-	((firstdiffsize=newsize-firstsize)); 
-	((mins=iterations/6)); 
-	
-	echo "${newsize} (${diffsize}) ($firstdiffsize - in $mins minutes)"; 
+INTERVAL=10;
+iterations=0;
+firstsize=0;
+oldsize=0;
 
-	oldsize="$newsize"; 
-	sleep 10; 
-	iterations=$((iterations +1)); 
+while true; do
+        newsize=`df | head -2 | tail -1 |  awk '{ print $3 }'`;
+
+        if [[ firstsize -eq 0 ]]; then
+                firstsize=$newsize;
+        fi;
+
+        ((diffsize=newsize - oldsize));
+        ((firstdiffsize=newsize-firstsize));
+        ((mins=(iterations*INTERVAL)/10));
+
+        echo "${newsize} (${diffsize}) ($firstdiffsize - in $mins minutes)";
+
+        oldsize="$newsize";
+        sleep $INTERVAL;
+        iterations=$((iterations +1));
 done
